1cf26d84e56cf13273edc133eaf29eaa0c132049
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-09-03 10:11:21 +0200

    feat: add backend config

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "rg_terraform_state_sapbtp"
-#    storage_account_name = "sasapbtptfstate"
-#    container_name       = "tfstate"
-#    key                  = "dev.terraform.tfstate"
-#  }
-#}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg_terraform_state_sapbtp"
+    storage_account_name = "sasapbtptfstate"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
