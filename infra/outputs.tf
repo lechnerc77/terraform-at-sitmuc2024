46d192ea8c61c4e76c3f5285e89ea263877087d2
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-09-03 10:07:32 +0200

    feat: base setup

---
+++ infra/outputs.tf
@@ -0,0 +1,9 @@
+output "url_sap_build_code" {
+  value       = module.build_code.url_sap_build_code
+  description = "URL of the SAP Build Code subscription"
+}
+
+output "url_sap_biz_app_studio" {
+  value       = module.build_code.url_sap_biz_app_studio
+  description = "URL of the SAP Business Application Studio subscription"
+}
