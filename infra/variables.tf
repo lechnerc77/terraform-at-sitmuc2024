e15855ee4585679da7875ee71fc1030cbf886f6b
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-10-03 10:43:42 +0200

    chore: set default stage to sanbox

--- infra/variables.tf
+++ infra/variables.tf
@@ -24,7 +24,7 @@ variable "region" {
 variable "stage" {
   type        = string
   description = "The stage of the subaccount."
-  default     = "DEV"
+  default     = "SBX"
 }
 
 variable "build_code_admins" {
